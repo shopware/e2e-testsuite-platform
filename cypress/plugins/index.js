94b65ef9ae1fccd5fa1c6b95ab965e9a49f3aa1d
Stephan Pohl <stp@shopware.com>
2020-01-07 15:31:58 +0100

    Fixes screenshot size too small on CI runs

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,8 +16,22 @@ const selectTestsWithGrep = require('cypress-select-tests/grep');
 const logToOutput = require('cypress-log-to-output');
 
 module.exports = (on, config) => {
+    logToOutput.install(on);
+
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
     on('file:preprocessor', selectTestsWithGrep(config));
-    logToOutput.install(on);
+
+    // See: https://github.com/cypress-io/cypress/issues/2102
+    on('before:browser:launch', (browser = {}, args) => {
+		if (browser.name === 'electron') {
+			args.width = 1920;
+			args.height = 1080;
+			return args;
+		}
+		if (browser.name === 'chrome') {
+			args.push('--window-size=1920,1080');
+			return args
+		}
+	});
 };
