ca19a3c64fbd6fbe96707281dbabe1cc55f6e78f
Stephan Pohl <s.pohl@shopware.com>
2020-02-14 15:39:08 +0100

    Bumps cypress to v4.0.1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,11 +13,8 @@
 
 require('@babel/register');
 const selectTestsWithGrep = require('cypress-select-tests/grep');
-const logToOutput = require('cypress-log-to-output');
 
 module.exports = (on, config) => {
-    logToOutput.install(on);
-
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
     on('file:preprocessor', selectTestsWithGrep(config));
