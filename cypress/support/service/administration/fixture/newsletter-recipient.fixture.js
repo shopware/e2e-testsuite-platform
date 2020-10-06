5374a913c607d0fe06443250a18d3e01d76bbb1c
Niklas Limberg <n.limberg@shopware.com>
2020-10-06 19:35:27 +0200

    complete Fixture

--- cypress/support/service/administration/fixture/newsletter-recipient.fixture.js
+++ cypress/support/service/administration/fixture/newsletter-recipient.fixture.js
@@ -1,21 +1,40 @@
 const AdminFixtureService = require('../fixture.service.js');
 
 class NewsletterRecipient extends AdminFixtureService {
-    setNewsletterRecipientFixture(customer) {
-        return this.getClientId()
-            .then((result) => {
-                this.apiClient.setAccessKey(result);
-            })
-            .then(() => {
-                return this.apiClient.post(`/${Cypress.env('apiVersion')}/customer/login`, JSON.stringify({
-                    username: customer.username,
-                    password: customer.password
-                }));
-            }).then((contextToken) => {
-                this.apiClient.setContextToken(contextToken['sw-context-token']).then(() => {
-                    this.apiClient.post('/widgets/account/newsletter')
-                });
-            })
+    setNewsletterRecipientFixture(recipient) {
+
+        cy.getSalesChannelId().then((accessKey) => {
+            const headers = {
+                "Content-Type": "application/json",
+                "SW-Access-Key": accessKey
+            };
+
+            function getSalutation() {
+                const url = '/sales-channel-api/v3/salutation';
+                return fetch(url, { method: 'GET', headers })
+                    .then((resp) => resp.json())
+                    .then((json) => json.data[0]);
+            }
+
+            function subscribeRecipient(recipient) {
+                const url = '/sales-channel-api/v3/newsletter/subscribe';
+                const body = JSON.stringify(recipient);
+                return fetch(url, { method: 'POST', headers, body })
+                    .then(response => {
+                        if (response.statusText !== "No Content") {
+                            throw new Error('Recipient creation failed')
+                        }
+                    });
+            }
+
+            return getSalutation().then(salutation => {
+                recipient['salutationId'] = salutation.id;
+                return subscribeRecipient(recipient);
+
+            });
+
+
+        })
     }
 }
 
