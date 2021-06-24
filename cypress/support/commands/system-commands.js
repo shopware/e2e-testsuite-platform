a3a7be9274e3f0fc9a1aead5ae8d615e97ef088c
Stephan Pohl <s.pohl@shopware.com>
2021-06-24 14:33:05 +0200

    chore(deps) - Updated dependencies and removed cypres-dark

--- cypress/support/commands/system-commands.js
+++ cypress/support/commands/system-commands.js
@@ -8,15 +8,8 @@ const fs = require('fs');
  * @function
  */
 Cypress.Commands.add('activateShopwareTheme', () => {
-    const isStyleLoaded = $head => $head.find('#cypress-dark').length > 0;
-    const themeFilename = 'node_modules/@shopware/e2e-testsuite/theme/shopware.css';
-
     // Cypress includes jQuery
     const $head = Cypress.$(parent.window.document.head); // eslint-disable-line no-restricted-globals
-    if (isStyleLoaded($head) || !Cypress.config('useShopwareTheme')) {
-        return;
-    }
-
     $head.append(
         `<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/brands.css" integrity="sha384-i2PyM6FMpVnxjRPi0KW/xIS7hkeSznkllv+Hx/MtYDaHA5VcF0yL3KVlvzp8bWjQ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/fontawesome.css" integrity="sha384-sri+NftO+0hcisDKgr287Y/1LVnInHJ1l+XC7+FOabmTTIK0HnE2ID+xxvJ21c5J" crossorigin="anonymous">
